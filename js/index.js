7886a9980d070a214f5189a2be37dcf60df03935
A <somer_kill@mail.ru>
2019-01-29 17:02:19 +0300

    Calculator works!
    - The index.js file has been updated. Buttons have actions to get the result.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,38 @@
-function plus() {
-alert(document.getElementsByTagid("a").value);
+// Get result of action.
+function getResult(action_name){
+
+    this.action_name = action_name; // Get action name.
+
+    let a = parseInt(document.getElementById("A").value); // Get values from tags of inputs.
+    let b = parseInt(document.getElementById("B").value);
+
+    let result = 0; // Set standart value for 'result'.
+
+    switch (this.action_name){ // Choosing action for A and B.
+        case '+': {
+            result = a + b;
+            break;
+        }
+        case '-': {
+            result = a - b;
+            break;
+        }
+        case '*': {
+            result = a * b;
+            break;
+        }
+        case '/': {
+            result = a / b;
+            break;
+        }
+    }
+
+    alert(`Результат: ${result}`); // Output of result.
+}
+
+// Clear inputs.
+function clearInputs(){
+
+    document.getElementById("A").value = null;
+    document.getElementById("B").value = null;
+}
